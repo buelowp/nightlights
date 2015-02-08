c4ff5e06f22e43f12e6dc86b37227dca780cd853
Peter Buelow <goballstate@gmail.com>
2015-02-08 12:17:53 -0600

    Fixed the pinpoint sketch to fade in and out.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nightlights
 Baby room Arduino night light implementations
+
+All of these are for the Neopixel shield sold by Adafruit
+
+http://www.adafruit.com/products/1430
+
+The idea is that I light the shield up in different patterns,
+and cover it with something that would then glow or show off
+patterns on the ceiling. Currently, we use a glass fishtank
+painted white, so the colors glow through. We put little
+star stickers inside, and peeled them off to allow us to 
+paint colored stars on the ceiling. The pinpoint ino will
+do this.
+
+Feel free to build them and use them if you would like.
--- pinpoint/pinpoint.ino
+++ pinpoint/pinpoint.ino
@@ -5,6 +5,8 @@
 #define BRIGHTNESS    255
 
 CRGB strip[NUM_LEDS];
+CHSV oldPixel;
+int oldLocation;
 
 HSVHue ColorWheel[] = {
   HUE_RED,
@@ -18,49 +20,82 @@ HSVHue ColorWheel[] = {
 };
 
 void setup() {
-  // put your setup code here, to run once:
   delay(3000);
   FastLED.addLeds<NEOPIXEL, PIN1>(strip, NUM_LEDS);
   randomSeed(analogRead(1));
   Serial.begin(9600);
+  oldLocation = 0;
 }
 
-void see_the_rainbow(CHSV &pixel, int index)
+void paint_canvas_black()
+{
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip[i] = CRGB::Black;
+  }
+  FastLED.show();
+}
+
+void swap_pixels(CHSV &pixel, int location)
 {
   CHSV pixels[NUM_LEDS];
-  CHSV black;
+  CHSV blackPixel;
+  int newPix[4];
+  int oldPix[4];
+  int pos;
   
-  if ((index % 8) == 7) {
-    index = index - 1;
-  }
+  blackPixel.v = 0;
+  blackPixel.s = 0;
+  blackPixel.h = 0;
   
-  black.v = 0;
-  black.h = 0;
-  black.s = 0;
+  paint_canvas_black();
   
+  newPix[0] = location;
+  newPix[1] = location + 1;
+  newPix[2] = location + 8;
+  newPix[3] = location + 9;
+  oldPix[0] = oldLocation;
+  oldPix[1] = oldLocation + 1;
+  oldPix[2] = oldLocation + 8;
+  oldPix[3] = oldLocation + 9;
+
   for (int i = 0; i < NUM_LEDS; i++) {
-    pixels[i] = black;
+    pixels[i] = blackPixel;
   }
-  pixels[index] = pixel;
-  pixels[index + 1] = pixel;
-  pixels[index + 8] = pixel;
-  pixels[index + 9] = pixel;
-  
-  hsv2rgb_rainbow(pixels, strip, NUM_LEDS);
-  FastLED.show();
+
+  while (pixel.v < 255) {
+    pixel.v++;
+    oldPixel.v--;
+    
+    for (int i = 0; i < 4; i++) {
+      pixels[oldPix[i]] = oldPixel;
+      pixels[newPix[i]] = pixel;
+    }
+
+    hsv2rgb_rainbow(pixels, strip, NUM_LEDS);
+    FastLED.show();
+    delay(20);
+  }
 }
 
 void loop() {
   int location = random(0, 31);
   CHSV pixel;
   
-  Serial.print("location is ");
-  Serial.println(location);
+  if (location == oldLocation) {
+    location = random(0, 31);
+  }
   
-  pixel.v = BRIGHTNESS;
+  if ((location % 8) == 7)
+    location++;
+    
+  pixel.v = 0;
   pixel.h = ColorWheel[random(0, 8)];
   pixel.s = 255;
-  
-  see_the_rainbow(pixel, location);
-  delay(5000);
+
+  swap_pixels(pixel, location);
+
+  oldPixel = pixel;
+  oldLocation = location;
+
+  delay(10000);
 }
